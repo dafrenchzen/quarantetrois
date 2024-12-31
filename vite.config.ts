d5da330e688725427e6b30b4f457d692ae37265d
dafrenchzen <dafreshtouch@gmail.com>
2024-12-31 20:19:05 +0100

    Fix build configuration and use relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/quarantetrois/',
   build: {
     outDir: 'dist',
+    emptyOutDir: true,
     rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
+      input: {
+        main: path.resolve(__dirname, 'index.html')
       }
+    }
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
     }
   }
 })
