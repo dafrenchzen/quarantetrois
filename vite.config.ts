d3dc7f2e98afdf951c6de3d1e0eb81bed0df7970
dafrenchzen <dafreshtouch@gmail.com>
2024-12-31 20:20:49 +0100

    Update build and deployment configuration

--- vite.config.ts
+++ vite.config.ts
@@ -7,10 +7,14 @@ export default defineConfig({
   base: '/quarantetrois/',
   build: {
     outDir: 'dist',
+    assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
-      input: {
-        main: path.resolve(__dirname, 'index.html')
+      input: path.resolve(__dirname, 'index.html'),
+      output: {
+        entryFileNames: `assets/[name].[hash].js`,
+        chunkFileNames: `assets/[name].[hash].js`,
+        assetFileNames: `assets/[name].[hash].[ext]`
       }
     }
   },
